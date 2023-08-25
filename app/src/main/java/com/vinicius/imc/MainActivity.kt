21b04aff34674f4fb535605da186aa8c337237a6
francovinicius <duartevini.dev@gmail.com>
2023-08-25 11:33:31 -0300

    add calculate button functionality

--- app/src/main/java/com/vinicius/imc/MainActivity.kt
+++ app/src/main/java/com/vinicius/imc/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 
@@ -34,6 +35,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         /* Calcular Imc */
+
+        findViewById<Button>(R.id.calcular).setOnClickListener {
+            //Captar informações do input
+
+            val altura = Integer.parseInt(findViewById<EditText>(R.id.altura).text.toString())
+            val peso = Integer.parseInt(findViewById<EditText>(R.id.peso).text.toString())
+
+            val calculo = peso/((altura * altura)/10000)
+
+            val resultado = findViewById<TextView>(R.id.resultado)
+
+            if (calculo < 18.5) {
+                resultado.setText("Seu resultado foi: ${calculo}" + "\n" + "Você esta abaixo do peso!")
+        } else {
+                resultado.setText("${calculo}" )
+            }
+        }
 
     }
 
